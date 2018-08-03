36733ada2f6ebf9536efd81ad4d115956fb213fc
JaneWB <brownjannick@att.net>
2018-08-03 15:25:09 -0500

    CG4-2 Week 4 Assignment Array

--- CG4-2/Program.cs
+++ CG4-2/Program.cs
@@ -6,7 +6,24 @@ namespace CG4_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Create and initialize in a single line an array with the following values: 1, 1, 2, 3, 5, 8.
+            //Then loop through the array and print out each value.
+            
+            //Create and array with integers in single line and initialize the array.
+            int[] array = new int[] { 1, 1, 2, 3, 5, 8 };
+
+            //Execute the loop according to the array criteria.
+            foreach (int i in array)
+            
+            //Print the array to the console.
+            Console.WriteLine(i);
+            Console.ReadLine();
+
+
+
+
+
+            
         }
     }
 }
